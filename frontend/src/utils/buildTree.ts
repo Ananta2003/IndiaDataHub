65420b23c40ef622024a2501118eab299cf45887
Ananta <anantavigave@gmail.com>
2025-11-30 19:54:30 +0530

    test done

--- frontend/src/utils/buildTree.ts
+++ frontend/src/utils/buildTree.ts
@@ -3,7 +3,9 @@ export interface TreeNode {
   children?: TreeNode[];
 }
 
-export function buildTree(obj: any): TreeNode[] {
+type NestedObject = { [key: string]: NestedObject };
+
+export function buildTree(obj: NestedObject): TreeNode[] {
   return Object.entries(obj).map(([key, value]) => ({
     name: key,
     children: Object.keys(value).length > 0 ? buildTree(value) : undefined
